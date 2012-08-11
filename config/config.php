cda241612dff867a533de66bba797812ea4d868a
Glen Langer <contao.modul@glen-langer.de>
2012-08-11 02:25:38 +0200

    Namespace und htaccess Anpassung fuer RC1

--- config/config.php
+++ config/config.php
@@ -20,7 +20,7 @@
 $GLOBALS['BE_MOD']['devtools']['dbsql_generator'] = array
 (
 	'callback'   => 'DatabaseGenerator\DBSQLGenerator',
-	'icon'       => 'system/modules/dbsql_generator/html/dbsqlgen.png',
+	'icon'       => 'system/modules/dbsql_generator/public/dbsqlgen.png',
 );
 
 ?>
